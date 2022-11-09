2280d7fbb4b6e803c76001efa66a16e376208692
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-11-09 13:02:09 +0600

    load data from mongo DB

--- index.js
+++ index.js
@@ -16,11 +16,29 @@ app.use(express.json());
 // 05 Database Connect > Connect your Application
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSAAWORD}@cluster0.athiem3.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+// 07 Async Function
+async function run() {
+  try {
+    const serviceCollection = client.db("lawyerWebsite").collection("services");
+
+    //07 Data load from Mongodb service to Client Side
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
 
 // 03 http://localhost:5000/  // server side access
 app.get("/", (req, res) => {
